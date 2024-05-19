ba5f3771585b0e9d45b6d5b4d8c4c96eaef03738
brilian <brillianmacha176@gmail.com>
2024-05-19 22:19:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/apiii22/TeamResponse.java
@@ -0,0 +1,10 @@
+package com.example.apiii22;
+
+import java.util.List;
+
+public class TeamResponse {
+    private List<Team> teams;
+    public List<Team> getTeams() {
+        return teams;
+    }
+}
---
+++ app/src/main/java/com/example/apiii22/TeamService.java
@@ -0,0 +1,9 @@
+package com.example.apiii22;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface TeamService {
+    @GET("search_all_teams.php?l=English%20Premier%20League")
+    Call<TeamResponse> getTeams();
+}
